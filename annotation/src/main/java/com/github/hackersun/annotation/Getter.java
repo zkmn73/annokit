ba2c648b208de01913a369911b8b697cc636a325
hackersun <hackersungl@gmail.com>
2015-12-20 15:23:39 +0800

    add Getter annotation

---
+++ annotation/src/main/java/com/github/hackersun/annotation/Getter.java
@@ -0,0 +1,17 @@
+package com.github.hackersun.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Desc:
+ * Author:sunguoli@meituan.com
+ * Date:15/12/20
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Getter {
+
+}
